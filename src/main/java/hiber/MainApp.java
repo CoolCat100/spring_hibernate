3f467a8f3943cf294dc6133ec612552c89c40e1b
CoolCat100 <xxdenxxx@mail.ru>
2024-04-13 23:55:41 +0300

    1

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -15,10 +15,12 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru", "lamborgini", 1));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru", "kia", 2));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru", "lada", 3));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru","ferrari", 4));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +28,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("model = "+user.getCar().getModel());
+         System.out.println("series = "+user.getCar().getSeries());
          System.out.println();
       }
+      User userByCar = userService.getUserByCar("kia", 2);
+      System.out.println(userByCar.getFirstName());
 
       context.close();
    }
